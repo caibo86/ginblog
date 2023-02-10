742a047cc5546075b567f9625a4b1e29a5aa39f2
bo <caibo@nd.com.cn>
2023-02-10 18:41:29 +0800

    logger

--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -1,4 +1,4 @@
-package logger
+package slog
 
 import (
 	"fmt"
@@ -11,18 +11,20 @@ var logger *logrus.Logger
 
 func init() {
 	logger = logrus.New()
+	logger.SetReportCaller(true)
+	logger.SetLevel(logrus.TraceLevel)
 	logger.SetFormatter(&logrus.TextFormatter{
 		FullTimestamp:   true,
 		TimestampFormat: "2006-01-02 15:03:04",
 		CallerPrettyfier: func(frame *runtime.Frame) (function string, file string) {
 			// fileName := fmt.Sprintf("%s:%d", frame.File, frame.Line)
 			fileName := fmt.Sprintf("%s:%d", path.Base(frame.File), frame.Line)
-			return frame.Function, fileName
+			return path.Base(frame.Function), fileName
 		},
 	})
 }
 
-// GetLogger 日志句柄获取
-func GetLogger() *logrus.Logger {
+// Default 日志句柄获取
+func Default() *logrus.Logger {
 	return logger
 }
